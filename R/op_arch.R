9f1fd1222728bbe9a0637e68e766bda3087792af
Joel Sebastian Salinas Barrera <jo-el_se808-bas.tan@outlook.com>
2022-03-28 02:14:59 -0600

    Editando parámetros función op_arch()
    Se agrega un espacio más para el parámetro vector "arch" para copiar o mover a la vez que se cambia el nombre, falta corregir y simplificar, también agregar documentación.

--- R/op_arch.R
+++ R/op_arch.R
@@ -1,33 +1,44 @@
 #' @title Operaciones con archivos
 #' @export
-op_arch <- function(op=NULL,arch=c("","",""),...){
+op_arch <- function(op=NULL,arch=c("","","",""),...){
   if (!is.null(op)) {
     #####-----------Identificando archivo-----------#####
-    if(op=="ren") nom <- arch[3] else if(op=="mov"|op=="cop"|op=="eli") nom <- arch[2]
-    ant <- list.files(.c[grep(nom,.c)],full.names = T) %>% .[grep(paste("/",arch[1],"\\.",sep = ""),.)]; ext <- stringr::str_extract(ant,"\\..*")
+    if (length(arch)==4) {
+      if(op=="mov"|op=="cop"){
+        ant <- list.files(.c[grep(arch[3],.c)],full.names = T) %>% .[grep(paste("/",arch[1],"\\.",sep = ""),.)]; ext <- stringr::str_extract(ant,"\\..*")
+        nue <- paste(.c[grep(arch[4],.c)],"/",arch[2],ext,sep="")
+      }
+    }else{
+      if(op=="ren") dir <- arch[3] else if(op=="mov"|op=="cop"|op=="eli") dir <- arch[2]
+      ant <- list.files(.c[grep(dir,.c)],full.names = T) %>% .[grep(paste("/",arch[1],"\\.",sep = ""),.)]; ext <- stringr::str_extract(ant,"\\..*")
+    }
     #####-----------Operacion-----------#####
     if (op=="ren") {
       nue <- paste(.c[grep(arch[3],.c)],"/",arch[2],ext,sep="")
       file.rename(ant,nue)
       print(paste("Archivo: ",ant,sep = ""))
       print(paste("Renombrado a: ",nue,sep=""))
+      print("")
     }
     else if (op=="mov") {
-      nue <- paste(.c[grep(arch[3],.c)],"/",arch[1],ext,sep="")
+      if(length(arch)<4) nue <- paste(.c[grep(arch[3],.c)],"/",arch[1],ext,sep="")
       file.rename(ant,nue)
       print(paste("Archivo: ",ant,sep = ""))
       print(paste("Movido a: ",nue,sep=""))
+      print("")
     }
     else if (op=="cop") {
-      nue <- paste(.c[grep(arch[3],.c)],"/",arch[1],ext,sep="")
+      if(length(arch)<4) nue <- paste(.c[grep(arch[3],.c)],"/",arch[1],ext,sep="")
       file.copy(ant,nue,overwrite = T,copy.date = T,...)
       print(paste("Archivo: ",ant,sep = ""))
       print(paste("Copiado o sobreescrito a: ",nue,sep=""))
+      print("")
     }
     else if (op=="eli") {
       file.remove(ant)
       print(paste("Archivo: ",ant,sep = ""))
       print("Eliminado")
+      print("")
     }
   }else if(op!="ren" & op!="mov" & op!="cop" & op!="eli"){print("Operacion incorrecta.")
   }else{print("No se especifica operacion a realizar con el archivo.")}
