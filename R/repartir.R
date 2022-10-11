07248a29c1bf6dcf04110e12db1c7a3e2b0320de
Joel Sebastian Salinas Barrera <jo-el_se808-bas.tan@outlook.com>
2022-10-11 13:18:29 -0500

    Arreglo función "repartir"
    Se arregla detalle con la función "repartir" que impedía repartir de manera correcta si se indicaba algún valor igual en "x" que en "reem".

--- R/repartir.R
+++ R/repartir.R
@@ -3,8 +3,8 @@
 #' No incluye una opcion para repartir una columna completa ya que se puede utilizar directamente rep_len() para esas situaciones.
 #' Los datos de reemlazo y los datos a reemplazar deben ser del mismo tipo.
 #' @param Base Dataframe, se retorna el mismo con la columna repartida
-#' @param x Vector de datos los cuales se van a repartir.
 #' @param col Columna del dataframe que se tomara para repartir el vector.
+#' @param x Vector de datos los cuales se van a repartir.
 #' @param reem Datos que se van a reemplazar de la columna dada, por defecto es "NA".
 #' @examples
 #' Base <- repartir(Base,c("A","B","C"),"columna")
@@ -12,16 +12,25 @@
 #' Base <- repartir(Base,c("A","B","C")"columna2",c("A","C","D"))
 #' @import utils dplyr
 #' @export
-repartir <- function(Base,x,col,reem=NA){
+repartir <- function(Base,col,x,reem=NA){
   #Conteo de faltantes
-  y <- as.data.frame(x) %>% `colnames<-`(col)%>%
-    left_join(filter(Base,Base[[col]] %in% x) %>% count(.[[col]]) %>% `colnames<-`(c(col,"n")),by=col)%>%
-    mutate(n=replace_na(n,0)) %>% mutate(n=max(n)-n) %>% arrange(desc(n))
+  y <- as.data.frame(x) %>% `colnames<-`(col) %>% left_join(filter(Base,!Base[[col]] %in% reem) %>% count(.[[col]]) %>% `colnames<-`(c(col,"n")),by=col)%>%
+    mutate(n=replace_na(n,0)) %>% arrange(n) %>% mutate(a=0)
   #Llenado completo
   noms <- c()
-  for (i in 1:(length(y[[1]])-1)) {
-    aux <- rep(y[[col]][i],y[["n"]][i])
-    noms <- c(noms,aux)
+  for (i in 1:length(y[[1]])) {
+    for (j in 1:length(y[[1]])) {
+      if(j!=length(y[[1]])){
+        if(y[["n"]][j]!=y[["n"]][j+1]){
+          y[["a"]][1:j] <- y[["n"]][j+1] - y[["n"]][j]
+          y[["n"]][1:j] <- y[["n"]][j] + y[["a"]][j]
+          noms <- c(noms,rep(y[[col]][1:j],y[["a"]][j]))
+          break
+        }
+      }else{
+        break
+      }
+    }
   }
   #Resta de sobrantes o faltantes
   n <- length(filter(Base,Base[[col]] %in% reem)[[1]]) - length(noms)
